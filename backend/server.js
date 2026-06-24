00adc643faa7ecb6aafa854f30308c51eca08932
kb-patel2005 <krishpatel1181@gmail.com>
2026-06-24 17:15:04 +0530

    add successfully,,,

--- backend/server.js
+++ backend/server.js
@@ -142,10 +142,10 @@ app.delete("/api/products/:id", async (req, res) => {
   }
 });
 
-//  app.use(express.static(path.join(__dirname, "frontend/dist")));
+ app.use(express.static(path.join(__dirname, "frontend/dist")));
 
-//  app.get(/.*/, (req, res) => {
-//    res.sendFile(path.join(__dirname, "frontend/dist", "index.html"));
-//  });
+ app.get(/.*/, (req, res) => {
+   res.sendFile(path.join(__dirname, "frontend/dist", "index.html"));
+ });
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
