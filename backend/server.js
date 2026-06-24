6bebf5334a4b4856bf35643a573d7c7f3b06f12f
kb-patel2005 <krishpatel1181@gmail.com>
2026-06-24 12:51:32 +0530

    all correct...

--- backend/server.js
+++ backend/server.js
@@ -61,5 +61,4 @@ app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, "frontend/dist", "index.html"));
 });
 
-const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
